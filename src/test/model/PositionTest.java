46a986740e754e738c0c96eb8c45c8915d4c8a1b
Enzo <etanyen@students.cs.ubc.ca>
2023-07-19 02:11:53 -0700

    Added a few more tests again.

--- src/test/model/PositionTest.java
+++ src/test/model/PositionTest.java
@@ -48,6 +48,25 @@ public class PositionTest {
         // Test for null.
         Position newPos2 = new Position(9, 11);
         assertFalse(newPos2.equals(null));
+
+        // More Test Cases:
+        Position equalNullPos = new Position(5, 10);
+        Position equalNullPos1 = new Position(0, 0);
+        assertFalse(equalNullPos.equals(equalNullPos1));
+
+        Position diffYPos = new Position(5, 15);
+        Position diffYPos1 = new Position(5, 10);
+        assertFalse(diffYPos.equals(diffYPos1));
+
+        Position diffXPos = new Position(3, 10);
+        Position diffXPos1 = new Position(5, 10);
+        assertFalse(diffXPos.equals(diffXPos1));
+
+        Position diffObj = new Position(1, 4);
+        String diffObj1 = "lol";
+        assertTrue(diffObj.equals(new Position(1, 4)));
+        assertTrue(diffObj1.equals("lol"));
+        assertFalse(diffObj.equals(diffObj1));
     }
 
 }
